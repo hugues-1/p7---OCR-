f7b702c4b896f5f60969ebb46dc18efb8a9fd862
hugues.hansen@gmail.com <77995307+hugues-1@users.noreply.github.com>
2023-03-28 18:32:16 +0200

    Premier envoi sur Github
    Premier envoi sur Github

---
+++ test_st.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import streamlit as st
+
+streamlit.run
+x = st.slider('Select a value')
+st.write(x, 'squared is', x * x)
+
+
+# In[ ]:
+
+
+
+
